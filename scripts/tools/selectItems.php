9bdc9416556ab91b2aaa245578aea58fc639450f
suFredericD <ilric6666@gmail.com>
2019-11-23 03:37:25 +0100

    Nouveau fichier : scripts/tools/selectItems.php 		Fonctions de sélection des questions

---
+++ scripts/tools/selectItems.php
@@ -0,0 +1,15 @@
+<?php
+/**************************************************************************************
+ *		EmtZahella Projetcs Inc.
+ *                Projet :   ToWaQ
+ *                  Page :   selectItems.php
+ *                Chemin :   https://127.0.0.1/ToWaQ/scripts/tools/selectItems.php
+ *                  Type :   page de scripts
+ *              Contexte :   Php 7.3
+ *              Fonction :   fonctions de sélection des questions
+ *   Date mise en oeuvre :   22/11/2019
+ *          Dernière MàJ :   22/11/2019
+ **************************************************************************************/
+/***** *****    INCLUSIONS ET SCRIPTS   ***** *****/
+
+?>
